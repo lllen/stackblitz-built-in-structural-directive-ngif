53109e0f526221c6cfcf762d2240d21007e86759
rozhko <olena1q2@gmail.com>
2024-02-26 15:46:35 +0200

    added example for switchCase

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,17 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-  arr = [1, 4, 62, 5, 2, 5];
-
-  generateNewNumber() {
-    this.arr.push(Math.floor(Math.random() * 100));
-  }
+  selectedValue = '';
+  options = ['Europe', 'South America', 'Asia', 'Australia'];
 }
