7be7f4152395e64f1fb6e8f3947f5b9dde843bf1
Olena Rozhko <olena_rozhko@epam.com>
2024-03-04 10:23:43 +0200

    added example of custom attr directive

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,17 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SomeFeatureModule } from "../feature.module";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SomeFeatureModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-  arr = [1, 4, 62, 5, 2, 5];
-
-  generateNewNumber() {
-    this.arr.push(Math.floor(Math.random() * 100));
-  }
 }
